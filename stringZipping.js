c7a385adffc511886e155a842ff24347a3576c22
HardLight00 <imazan00@gmail.com>
2019-11-06 09:35:39 +0300

    Refactor code

--- stringZipping.js
+++ stringZipping.js
@@ -6,16 +6,15 @@ const zipString = str => {
     let zipped = '',
         current = str[0],
         count = 0;
-    for (const index in str.split('')) {
-        const char = str[index];
-        if (char === current)
-            count++;
-        else {
+    [...str].forEach(char => {
+        if (char !== current) {
             zipped += count > 1 ? `${current}${count}` : current;
             current = char;
             count = 1;
+        } else {
+            count++;
         }
-    }
+    });
 
     return count > 1 ? `${zipped}${current}${count}` : zipped + current;
 };
